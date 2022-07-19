3fec392e364516dbab72dcfb24155948180f5aeb
worldofmarcus <me@marcuseriksson.com>
2022-07-19 09:29:32 +0000

    Change header size in media queries

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,5 @@
+// With help from 'Online Tutorials' on Youtube - https://www.youtube.com/watch?v=7Vr1bngah-k (Add Remove Active Class On Click - Html CSS and Javascript)
+
 $(document).on('click', 'ul li', function () {
-
     $(this).addClass('active').siblings().removeClass('active')
-
 })
